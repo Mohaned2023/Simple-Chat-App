2c7e4a1ef03ef7498b6969e8df6ca03f4131e91f
Mohaned2023 <shrhanmhnd15@gmail.com>
2025-03-04 20:26:59 +0300

    Added create conversation

--- client/commands/handler.py
+++ client/commands/handler.py
@@ -1,9 +1,13 @@
+import requests
+from config import Config
+
 class CommandHandler:
     def __init__(self, app):
         self.app = app
 
     def execute(self, command: str):
         command = command.strip()
+        args: list[str] = command.split(' ')
         if command.startswith(":chats"):
             self.app.switch_screen("conversations")
         elif command.startswith(":chat"):
@@ -15,7 +19,50 @@ class CommandHandler:
         elif command.startswith(":update"):
             self.app.switch_screen("update")
         elif command.startswith(":info"):
-            args: list[str] = command.split(' ')
             self.app.switch_screen("user_info", username= None if len(args) < 2 else args[1] )
+        elif command.startswith(":create"):
+            if len(args) < 2:
+                self.app.notify(
+                    "The create command needed to have the user name\n"
+                    "Please use the command like: `:create <username>`.",
+                    title="Not Found Error",
+                    severity="error"
+                )
+                return
+            self.createConversation(args[1])
         else: 
             ...
+    
+    def createConversation(self, username:str ):
+        accessToken, _ = Config.get_tokens()
+        res = requests.post(
+            Config.CREATE_CONVERSATION.replace(":username", username),
+            headers={ 'Authorization': accessToken }
+        )
+        if res.status_code == 201:
+            res_data: dict = res.json()
+            self.app.notify(
+                f"ID: {res_data['id']}\n"
+                f"users: @{res_data['user1']} - @{res_data['user2']}",
+                title="Conversation Has Been Created.",
+                severity="information"
+            )
+        elif res.status_code == 401:
+            self.app.notify(
+                "Please make sure that you are logged in.\n"
+                "If no try the command `:login` or `:register`.\n",
+                title="Unauthorized Error!",
+                severity="error"
+            )
+        elif res.status_code == 404:
+            self.app.notify(
+                f"User `{username}` NOT found.\n",
+                title="Not Found Error",
+                severity="error"
+            )
+        elif res.status_code in [429, 500]:
+            self.app.notify(
+                "Too many requests! Please try again later.",
+                title="Server Error!",
+                severity="error"
+            )
